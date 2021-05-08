4408cf05a3f6d46107770e8848f74172d11fc799
contractorakshar <contractorakshar8319@gmail.com>
2021-05-08 14:43:23 +0530

    done

--- server.js
+++ server.js
@@ -1,22 +1,44 @@
-
-const express = require('express');
-const mongoose =require('mongoose');
-const articleRouter = require('./routes/article');
-const methodOverride=require('method-override');
+const express = require("express");
+const mongoose = require("mongoose");
+const articleRouter = require("./routes/article");
+const methodOverride = require("method-override");
 const app = express();
-const Articles=require('./models/article')
-mongoose.connect('mongodb://localhost/blog',{
-    useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true
-})
-app.set('view engine','ejs');
-
-
-app.use(express.urlencoded({extended:false}))
-app.use(methodOverride('_method'))
-app.get('/',async (req,res)=>{
-    // res.send('Hello world');
-    const article= await Articles.find().sort({createdAt:'desc'})
-    res.render('articles/index',{articles:article});
+const Url = require("./myUrl");
+const Articles = require("./models/article");
+const fetch = require("node-fetch");
+mongoose.connect(Url.mongoDB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
-app.use('/articles',articleRouter);
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+
+app.get("/", async (req, res) => {
+  // let a = "happy";
+  // // res.send('Hello world');
+  // fetch("https://aplet123-wordnet-search-v1.p.rapidapi.com/master?word=" + a, {
+  //   method: "GET",
+  //   headers: {
+  //     "x-rapidapi-key": "d00d745d82msh49a483b75ee89fdp14464ajsneb0658917638",
+  //     "x-rapidapi-host": "aplet123-wordnet-search-v1.p.rapidapi.com",
+  //   },
+  // })
+  //   .then((res) => res.json())
+  //   .then((data) => {
+  //     let d = JSON.stringify(data.definition);
+  //     //   console.log(d.split(";"));
+  //     let arr = d.split(";");
+  //     res.send(arr[0]);
+  //   })
+  //   .catch((err) => {
+  //     console.error(err);
+  //   });
+
+  const article = await Articles.find().sort({ createdAt: "desc" });
+  res.render("articles/index", { articles: article });
+});
+app.use("/articles", articleRouter);
 app.listen(3000);
