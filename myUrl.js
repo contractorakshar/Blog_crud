637f43ee06a50e6a592c88b687e9450b534cc8f5
contractorakshar <contractorakshar8319@gmail.com>
2021-05-08 17:16:01 +0530

    done

--- myUrl.js
+++ myUrl.js
@@ -2,3 +2,22 @@ module.exports = myUrl = {
   mongoDB:
     "mongodb+srv://Akshar:akshar@2021@cluster0.begp6.mongodb.net/Blog?retryWrites=true&w=majority",
 };
+// let a = "happy";
+// // res.send('Hello world');
+// fetch("https://aplet123-wordnet-search-v1.p.rapidapi.com/master?word=" + a, {
+//   method: "GET",
+//   headers: {
+//     "x-rapidapi-key": "d00d745d82msh49a483b75ee89fdp14464ajsneb0658917638",
+//     "x-rapidapi-host": "aplet123-wordnet-search-v1.p.rapidapi.com",
+//   },
+// })
+//   .then((res) => res.json())
+//   .then((data) => {
+//     let d = JSON.stringify(data.definition);
+//     //   console.log(d.split(";"));
+//     let arr = d.split(";");
+//     res.send(arr[0]);
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
--- server.js
+++ server.js
@@ -6,37 +6,17 @@ const app = express();
 const Url = require("./myUrl");
 const Articles = require("./models/article");
 const fetch = require("node-fetch");
-// mongoose.connect(Url.mongoDB, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useCreateIndex: true,
-// });
+mongoose.connect("mongodb://localhost:27017/blog", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
 
 app.get("/", async (req, res) => {
-  // let a = "happy";
-  // // res.send('Hello world');
-  // fetch("https://aplet123-wordnet-search-v1.p.rapidapi.com/master?word=" + a, {
-  //   method: "GET",
-  //   headers: {
-  //     "x-rapidapi-key": "d00d745d82msh49a483b75ee89fdp14464ajsneb0658917638",
-  //     "x-rapidapi-host": "aplet123-wordnet-search-v1.p.rapidapi.com",
-  //   },
-  // })
-  //   .then((res) => res.json())
-  //   .then((data) => {
-  //     let d = JSON.stringify(data.definition);
-  //     //   console.log(d.split(";"));
-  //     let arr = d.split(";");
-  //     res.send(arr[0]);
-  //   })
-  //   .catch((err) => {
-  //     console.error(err);
-  //   });
-
   const article = await Articles.find().sort({ createdAt: "desc" });
   res.render("articles/index", { articles: article });
 });
